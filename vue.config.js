8cd9fff9cf4dff99933878ee38f6c73545d8e944
Abin <wb519@vip.qq.com>
2024-09-20 17:00:30 +0800

    初始化工程

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = {
       '/api': {
         // target: 'http://192.168.131.194:8097/', // 设置代理的目标URL
         target: 'http://110.41.58.26:8080/',
+        // target: 'http://localhost:8080/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '' // 将请求中的/api路径替换为空
